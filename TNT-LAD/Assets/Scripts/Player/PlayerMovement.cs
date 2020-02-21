5d28852b403a575fe5ab7096f5a6ae62642cdf3f
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-02-21 22:33:15 +0100

    Added experimental base for Player joining, working with keyboard or controller

---
+++ TNT-LAD/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public float walkSpeed;
+  private Vector2 axis;
+
+  void Update()
+  {
+    MovePlayer();
+  }
+
+  void MovePlayer()
+  {
+    Vector3 movement = new Vector3(axis.x, 0, axis.y) * walkSpeed;
+    transform.Translate(movement * Time.deltaTime);
+  }
+
+  void OnMovement(InputValue inputValue)
+  {
+    axis = inputValue.Get<Vector2>();
+  }
+}
