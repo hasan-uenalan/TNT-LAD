d4a8adbc0cbd98732f1e6b8c4dd61ededc25baf8
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-02-26 12:45:08 +0100

    Bombs can be placed, changed Player Rigidbody to CharacterController

---
+++ TNT-LAD/Assets/Scripts/Player/BombDropper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDropper : MonoBehaviour
+{
+  public GameObject bomb;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  void OnDropBomb()
+  {
+    Vector3 placementPosition = new Vector3(
+        Mathf.RoundToInt(gameObject.transform.position.x), 
+        0.5f, 
+        Mathf.RoundToInt(gameObject.transform.position.z)
+      );
+    foreach(var collider in Physics.OverlapSphere(placementPosition, 0.01f))
+    {
+      if(collider.gameObject.tag == "Bomb")
+      {
+        Debug.Log($"Bomb already on: {placementPosition}");
+        return; //Don't drop Bomb if one is already there.
+      }
+    }
+    Instantiate(bomb, placementPosition, Quaternion.identity);
+  }
+}
--- TNT-LAD/Assets/Scripts/Player/PlayerMovement.cs
+++ TNT-LAD/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
 {
   public float walkSpeed;
   private Vector2 axis;
+  CharacterController characterController;
+
+  private void Start()
+  {
+    characterController = gameObject.GetComponent<CharacterController>();
+  }
 
   void Update()
   {
@@ -16,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
   void MovePlayer()
   {
     Vector3 movement = new Vector3(axis.x, 0, axis.y) * walkSpeed;
-    transform.Translate(movement * Time.deltaTime, Space.World);
+    characterController.Move(movement * Time.deltaTime);
 
     Vector3 newDirection = Vector3.RotateTowards(transform.forward, new Vector3(axis.x, 0, axis.y), Time.deltaTime * 20f, 0.0f);
     transform.rotation = Quaternion.LookRotation(newDirection * Time.deltaTime);
