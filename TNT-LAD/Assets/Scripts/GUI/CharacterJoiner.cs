7d97704caeb7d015536558e31a085429cb666253
TvRXVII <timovonrauchhaupt@gmail.com>
2020-02-26 15:13:34 +0100

    Added Player selection Script

--- TNT-LAD/Assets/Scripts/GUI/CharacterJoiner.cs
+++ TNT-LAD/Assets/Scripts/GUI/CharacterJoiner.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterJoiner : MonoBehaviour
 {
-  // Start is called before the first frame update
-  void Start()
-  {
+  public List<GameObject> PlayerList;
 
+  private int playerCount = 1;
+
+  private void Start()
+  {
+    PlayerList = new List<GameObject>();
   }
 
   // Update is called once per frame
   void Update()
   {
+    if (Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.Joystick1Button10)) 
+    {
+      PlayerList.Add(ActivateNewPlayer());
 
+    }
+  }
+
+  private GameObject ActivateNewPlayer()
+  {
+    foreach (GameObject curPlayerSelection in GameObject.FindGameObjectsWithTag("GUIPlayer")) 
+    {
+      LobbyPlayerValues valuesOfCurPlayer = curPlayerSelection.GetComponent<LobbyPlayerValues>();
+      if (!valuesOfCurPlayer.getIsSelected())
+      {
+        valuesOfCurPlayer.SetIsSelected(true);
+        curPlayerSelection.transform.GetChild(0).GetComponent<Text>().text = "Player " + playerCount;
+        playerCount++;
+        curPlayerSelection.transform.GetChild(1).GetComponent<Image>().enabled = false;
+        return curPlayerSelection;
+      }
+    }
+    return null;
   }
 }
---
+++ TNT-LAD/Assets/Scripts/GUI/LobbyPlayerValues.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyPlayerValues : MonoBehaviour
+{
+  private bool IsSelectedByPlayer { get; set; }
+
+  private void Start()
+  {
+    SetIsSelected(false);
+  }
+
+  public bool getIsSelected()
+  {
+    return IsSelectedByPlayer;
+  }
+
+  public void SetIsSelected(bool isSelectedByPlayer)
+  {
+    IsSelectedByPlayer = isSelectedByPlayer;
+  }
+
+}
