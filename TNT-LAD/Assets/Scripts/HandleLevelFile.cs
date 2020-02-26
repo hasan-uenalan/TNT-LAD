f84ebdff7fd4f3579812d0a041b6082dff71e42a
Banerry <cracky_x3@hotmail.com>
2020-02-26 10:40:45 +0100

    removed debuglog

--- TNT-LAD/Assets/Scripts/HandleLevelFile.cs
+++ TNT-LAD/Assets/Scripts/HandleLevelFile.cs
@@ -15,7 +15,6 @@ public class HandleLevelFile : MonoBehaviour
       levelController = gameObject.GetComponent<LevelController>();
 
       InitPath();
-      Debug.Log(Application.dataPath);
       var sr = new StreamReader(filePath);
       var fileContents = sr.ReadToEnd();
       sr.Close();
