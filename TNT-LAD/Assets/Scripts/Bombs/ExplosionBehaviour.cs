8b71c60ce790ad6622f503d6190dc5e5526e79a0
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-02-28 21:03:20 +0100

    Explosions now have dynamic colliders

---
+++ TNT-LAD/Assets/Scripts/Bombs/ExplosionBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBehaviour : MonoBehaviour
+{
+  public float explosionRadius;
+  public float expansionSpeed;
+  public float expansionOnsetDelay;
+  
+  private SphereCollider explosionCollider;
+  private bool isExploding;
+
+  void Start()
+  {
+    explosionCollider = gameObject.GetComponent<SphereCollider>();
+    Invoke("StartExplosion", expansionOnsetDelay);
+  }
+
+  private void Update()
+  {
+    if (isExploding)
+    {
+      DoExplosionExpansion();
+    }
+  }
+
+  void DoExplosionExpansion()
+  {
+    //Expand collider unil final radius is reached
+    if (explosionCollider.radius < explosionRadius)
+    {
+      explosionCollider.radius += Time.deltaTime * expansionSpeed;
+      return;
+    }
+    StopExplosion();
+  } 
+
+  private void StartExplosion()
+  {
+    explosionCollider.enabled = true;
+    isExploding = true;
+  }
+
+  private void StopExplosion()
+  {
+    isExploding = false;
+    explosionCollider.enabled = false;
+  }
+}
