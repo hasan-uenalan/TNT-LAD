4fdc4e9204e58b5e4ef93ad0fc7826f862dd1fdf
Banerry <cracky_x3@hotmail.com>
2020-03-03 12:51:01 +0100

    added auto camera positioning on play

---
+++ TNT-LAD/Assets/Scripts/Camera/CameraPositioning.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositioning : MonoBehaviour
+{
+  public Transform cam;
+  public float distance;
+
+  private LevelController levelController;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    levelController = gameObject.GetComponent<LevelController>();
+    cam.position = new Vector3(levelController.gridX / 2, distance, levelController.gridZ / 2);
+  }
+}
