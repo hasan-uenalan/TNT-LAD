a90e9f5897283a2cd11121f9175afd8304b042b9
TvRXVII <timovonrauchhaupt@gmail.com>
2020-03-03 15:51:59 +0100

    changed imports

--- TNT-LAD/Assets/Scripts/GUI/CharacterJoiner.cs
+++ TNT-LAD/Assets/Scripts/GUI/CharacterJoiner.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CharacterJoiner : MonoBehaviour
