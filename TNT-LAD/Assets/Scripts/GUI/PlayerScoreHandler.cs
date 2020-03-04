1a0547f23b06b709e1f4c04951c44cb1d1a6457c
TvRXVII <timovonrauchhaupt@gmail.com>
2020-03-04 11:23:50 +0100

    Only Players will be shown in Scoreboard

--- TNT-LAD/Assets/Scripts/GUI/PlayerScoreHandler.cs
+++ TNT-LAD/Assets/Scripts/GUI/PlayerScoreHandler.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayerScoreHandler : MonoBehaviour
 {
+
+  private int playerCount;
+
   void Start()
   {
-
+    playerCount = 4;//GUIValues.PlayerList.Count;
+    for (int i = 0; i < playerCount; i++) {
+      gameObject.transform.GetChild(i).gameObject.SetActive(true);
+    }
   }
 
   void Update()
