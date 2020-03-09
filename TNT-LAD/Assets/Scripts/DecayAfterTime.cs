8cd382f0c67595e742f28c3b396af528d2889d24
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-03-09 11:15:40 +0100

    Parts of destroyed destructible fade away after some time

---
+++ TNT-LAD/Assets/Scripts/DecayAfterTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecayAfterTime : MonoBehaviour
+{
+  public float lifeTime;
+  Transform destroyedTransform;
+
+  void Start()
+  {
+    destroyedTransform = gameObject.transform;
+    StartCoroutine(StartLifeTime());
+  }
+
+  public IEnumerator StartLifeTime()
+  {
+    yield return new WaitForSeconds(lifeTime);
+    
+    //Fade out animation
+    float elapsedTime = 0f;
+    float fadeTime = 2f;
+    while(elapsedTime < fadeTime)
+    {
+      elapsedTime += Time.deltaTime;
+      foreach(Transform boxFragment in destroyedTransform)
+      {
+        Color objColor = boxFragment.gameObject.GetComponent<MeshRenderer>().material.color;
+        objColor.a = Mathf.SmoothStep(1f, 0f, (elapsedTime / fadeTime));
+        boxFragment.gameObject.GetComponent<MeshRenderer>().material.color = objColor;
+      }
+      yield return null;
+    }
+    Destroy(gameObject);
+  }
+}
