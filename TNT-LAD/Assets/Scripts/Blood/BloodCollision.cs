709d26cb64466f372577a979b643d5d381420a72
Banerry <cracky_x3@hotmail.com>
2020-03-11 14:07:03 +0100

    added collision detection

---
+++ TNT-LAD/Assets/Scripts/Blood/BloodCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodCollision : MonoBehaviour
+{
+
+  public ParticleSystem PSystem;
+  private List<ParticleCollisionEvent> CollisionEvents;
+
+
+  public void OnParticleCollision(GameObject other)
+  {
+    CollisionEvents = new List<ParticleCollisionEvent>();
+
+
+    int eventCount = PSystem.GetCollisionEvents(other, CollisionEvents);
+
+    foreach(ParticleCollisionEvent collision in CollisionEvents)
+    {
+      Debug.Log(collision.intersection);
+    }
+
+  }
+}
