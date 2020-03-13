ce6e975d5f56231a4414d3706e6a032f12fe2b4d
Banerry <1+Banerry@users.noreply.github.comhub.com>
2020-03-13 22:27:03 +0100

    added exploding chests to start menu

--- TNT-LAD/Assets/Scripts/MenuBackgrounds/EnvironmentEvents.cs
+++ TNT-LAD/Assets/Scripts/MenuBackgrounds/EnvironmentEvents.cs
@@ -7,6 +7,7 @@ public class EnvironmentEvents : MonoBehaviour
   public Vector3 bombSpawn1;
   public Vector3 bombSpawn2;
   public GameObject bomb;
+  public GameObject destructibleBox;
 
   void Start()
   {
@@ -19,7 +20,7 @@ public class EnvironmentEvents : MonoBehaviour
     {
       yield return new WaitForSeconds(Random.Range(4, 8));
       Vector3 spawn = (Random.Range(0, 2) == 0) ? bombSpawn1 : bombSpawn2;
-
+      Instantiate(destructibleBox, spawn, Quaternion.identity);
       GameObject bombObj = Instantiate(bomb, spawn, Quaternion.identity);
       if(bombObj.TryGetComponent(out BombExploder bombExploder))
       {
