d8593fe03cab660f6de3ca193d13dfed88f88e0d
TvRXVII <timovonrauchhaupt@gmail.com>
2020-03-21 11:48:25 +0100

    UI is displayed and updated
    - Lifes are dynamicly displayed
    - Score is shown
    - PlayerImage is missing

---
+++ TNT-LAD/Assets/Scripts/GUI/IngameUI/PlayerUIElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUIElement
+{
+  public GameObject PlayerUI { get; private set; }
+
+  public int PlayerIndex { get; private set; }
+
+  public PlayerUIElement(int playerIndex, GameObject playerUI)
+  {
+    PlayerIndex = playerIndex;
+    PlayerUI = playerUI;
+  }
+}
---
+++ TNT-LAD/Assets/Scripts/GUI/IngameUI/UIHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+
+  public GameObject PlayerUI;
+  
+  private List<PlayerUIElement> playerUIs;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    playerUIs = new List<PlayerUIElement>();
+    foreach (PlayerData curPlayer in StaticPlayers.Players) 
+    {
+      GameObject curPlayerUI = GameObject.Instantiate(PlayerUI, gameObject.transform);
+      curPlayerUI.transform.localPosition = new Vector3(40f + (curPlayer.PlayerIndex * 110), -18f, 0f);
+      curPlayerUI.transform.GetChild(1).GetComponent<Text>().text = "P" + curPlayer.PlayerIndex;
+      curPlayerUI.transform.GetChild(3).GetComponent<Text>().text = curPlayer.PlayerScore.ToString();
+      playerUIs.Add(new PlayerUIElement(curPlayer.PlayerIndex, curPlayerUI));
+    }
+  }
+
+  private void Update()
+  {
+    foreach(PlayerUIElement curPlayerElement in playerUIs) 
+    {
+      foreach (PlayerData curPlayerData in StaticPlayers.Players) 
+      {
+        if (curPlayerElement.PlayerIndex == curPlayerData.PlayerIndex) {
+          curPlayerElement.PlayerUI.transform.GetChild(2).GetComponent<Text>().text = curPlayerData.Lifes.ToString();
+        }
+      }
+    }
+  }
+}
