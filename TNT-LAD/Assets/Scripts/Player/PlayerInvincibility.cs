5f726cbe579db2f3fdc126087ebc051ec6c91c8e
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-03-24 16:34:56 +0100

    Added visual effect when player is invincible

---
+++ TNT-LAD/Assets/Scripts/Player/PlayerInvincibility.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInvincibility : MonoBehaviour
+{
+  public GameObject shieldGroup;
+  public float rotationSpeed;
+  private PlayerHandler playerHandler;
+  private Quaternion fixRotation; 
+
+  void Start()
+  {
+    playerHandler = GetComponent<PlayerHandler>();
+  }
+
+  void Update()
+  {
+    if(playerHandler.PlayerStatus == PlayerHandler.Status.invincible)
+    {
+      shieldGroup.SetActive(true);
+      shieldGroup.transform.rotation = fixRotation;
+      shieldGroup.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+      fixRotation = shieldGroup.transform.rotation;
+      return;
+    }
+    shieldGroup.SetActive(false);
+  }
+}
