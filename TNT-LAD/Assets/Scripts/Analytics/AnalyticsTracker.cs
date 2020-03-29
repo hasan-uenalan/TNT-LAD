75c354bba8678469652ad5c6b1d87795e2a417f9
Banerry <1+Banerry@users.noreply.github.comhub.com>
2020-03-29 16:45:43 +0200

    added tracker basics

---
+++ TNT-LAD/Assets/Scripts/Analytics/AnalyticsTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsTracker : MonoBehaviour
+{
+  // Start is called before the first frame update
+
+  private int test = 0;
+
+  void Start()
+  {
+    AnlGameStarted();
+  }
+  
+  private void AnlGameStarted()
+  {
+    Analytics.EnableCustomEvent("gameStarted", true);
+    Analytics.CustomEvent("gameStarted", Vector3.zero);
+    Debug.Log("Custom Event started");
+  }
+
+}
