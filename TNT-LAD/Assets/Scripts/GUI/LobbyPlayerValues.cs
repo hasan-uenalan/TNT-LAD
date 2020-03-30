454306de44a8ef9c88eae2df2c4b2a7fa4c06cc5
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-03-30 23:30:24 +0200

    Players can now customize their characters hat color
    Hat color is not visible in-game yet

--- TNT-LAD/Assets/Scripts/GUI/LobbyPlayerValues.cs
+++ TNT-LAD/Assets/Scripts/GUI/LobbyPlayerValues.cs
@@ -5,23 +5,17 @@ using UnityEngine.InputSystem;
 
 public class LobbyPlayerValues
 {
-  public int PlayerNumber { get; set; }
-
-  public bool IsSelectedByPlayer { get; set; }
-
+  public Color PlayerColor { get; set; }
   public InputDevice PlayerInputDevice { get; set; }
-
   public GameObject JoinPlayerGameObject { get; set; }
 
   public LobbyPlayerValues()
   {
-    IsSelectedByPlayer = false;
+
   }
 
   public void RestoreDefault()
   {
-    PlayerNumber = 0;
-    IsSelectedByPlayer = false;
     PlayerInputDevice = null;
     JoinPlayerGameObject = null;
   }
---
+++ TNT-LAD/Assets/Scripts/Player/PlayerCustomization.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCustomization : MonoBehaviour
+{
+  public List<Color> playerColors;
+  [HideInInspector] public Material hatMat;
+  private int colorIndex;
+
+  void Start()
+  {
+    hatMat = gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material;
+  }
+
+  private void ColorDown()
+  {
+    colorIndex--;
+    if (colorIndex < 0)
+    {
+      colorIndex = playerColors.Count - 1;
+    }
+    hatMat.color = playerColors[colorIndex];
+  }
+
+  private void ColorUp()
+  {
+    colorIndex++;
+    if(colorIndex > playerColors.Count - 1)
+    {
+      colorIndex = 0;
+    }
+    hatMat.color = playerColors[colorIndex];
+
+  }
+
+  void OnColorSelectionUp(InputValue inputValue)
+  {
+    var input = inputValue.Get<float>();
+    if(input != 0f)
+    {
+      ColorUp();
+    }
+  }
+
+  void OnColorSelectionDown(InputValue inputValue)
+  {
+    var input = inputValue.Get<float>();
+    if (input != 0f)
+    {
+      ColorDown();
+    }
+  }
+}
