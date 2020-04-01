c4daa222779b4542f20efdb9c44c84b9f4b815bb
TvRXVII <timovonrauchhaupt@gmail.com>
2020-04-01 19:21:26 +0200

    Added Scripts to Scene

--- TNT-LAD/Assets/Scripts/GUI/OptionsUI/LifeUIHandler.cs
+++ TNT-LAD/Assets/Scripts/GUI/OptionsUI/LifeUIHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeHandler : MonoBehaviour
+public class LifeUIHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
