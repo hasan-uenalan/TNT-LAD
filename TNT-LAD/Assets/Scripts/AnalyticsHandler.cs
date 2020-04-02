76fa6643a04d8464e59b54430dd1f48d3702152b
Banerry <1+Banerry@users.noreply.github.comhub.com>
2020-04-02 10:35:14 +0200

    added analytics

---
+++ TNT-LAD/Assets/Scripts/AnalyticsHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsHandler : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+    Analytics.CustomEvent("gameStarted");
+  }
+
+  private void lol()
+  {
+    //Analytics.CustomEvent("lol", new Dictionary<string, Vector3>
+    //{
+    //  { },
+    //  { }
+    //});
+  }
+  
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
