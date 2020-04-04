35019bb04950923b9a96359815ebcb78bd43efe5
TvRXVII <timovonrauchhaupt@gmail.com>
2020-04-04 14:11:40 +0200

    created Reading and writing of OptionsFile

---
+++ TNT-LAD/Assets/Scripts/GUI/MainMenu/FileLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// is only called at the start of the Application!
+/// </summary>
+public class FileLoader : MonoBehaviour
+{
+  private static string directory;
+
+  [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+  static void SaveValuesFromOptionsFile()
+  {
+    directory = Path.Combine(Application.dataPath, "Resources/LevelFiles");
+    TextAsset optionsFile = (TextAsset) Resources.Load("Options");
+    if (optionsFile == null)
+    {
+      CreateNewOptionsFile();
+    }
+    SetOptionsFromFile(optionsFile);
+  }
+
+  private static void CreateNewOptionsFile()
+  {
+    string optionsJson = JsonUtility.ToJson(new OptionsData());
+    File.WriteAllText(Path.Combine(directory, "Options.txt"), optionsJson);
+  }
+
+  private static void SetOptionsFromFile(TextAsset optionsFile)
+  {
+    OptionsData optionsData = OptionsData.CreateOptionsData(optionsFile);
+    CrossSceneInformation.PlayerLifes = optionsData.PlayerLifes;
+    CrossSceneInformation.RoundTime = optionsData.RoundTime;
+  }
+}
---
+++ TNT-LAD/Assets/Scripts/GUI/MainMenu/OptionsData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class OptionsData
+{
+  public int RoundTime{ get; private set; }
+
+  public int PlayerLifes { get; private set; }
+
+  public OptionsData()
+  {
+    RoundTime = 300;
+    PlayerLifes = 3;
+  }
+
+  public static OptionsData CreateOptionsData(TextAsset optionsFile)
+  {
+    return JsonUtility.FromJson<OptionsData>(optionsFile.text);
+  }
+}
