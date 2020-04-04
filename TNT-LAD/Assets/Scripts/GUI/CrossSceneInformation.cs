b83bcbef6bf9b23b38d6bbd57a387b1805a7064e
TvRXVII <timovonrauchhaupt@gmail.com>
2020-04-04 14:12:54 +0200

    Deleted Default Values

--- TNT-LAD/Assets/Scripts/GUI/CrossSceneInformation.cs
+++ TNT-LAD/Assets/Scripts/GUI/CrossSceneInformation.cs
@@ -7,7 +7,7 @@ public static class CrossSceneInformation
 {
   public static LevelInfo currentLevel { get; set; } = new LevelInfo();
   public static List<LobbyPlayerValues> PlayerList { get; set; }
-  public static int RoundTime { get; set; } = 300; //TODO: File
-  public static int PlayerLifes { get; set; } = 3; //TODO: File
+  public static int RoundTime { get; set; }
+  public static int PlayerLifes { get; set; }
 
 }
