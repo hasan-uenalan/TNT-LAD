34a85fda2852e3be5854fb9bde5ba806e5e61454
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-04-07 13:40:30 +0200

    New scoreboard display

---
+++ TNT-LAD/Assets/Scripts/GUI/PostGameUI/ScoreBoardHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreBoardHandler : MonoBehaviour
+{
+  [HideInInspector] public int WinCondition = 3; //Hidden because it should not be edited from inspector. (Values < 3 not supported by shelf GameObject)
+  public List<GameObject> playerShelves;
+
+  void Start()
+  {
+    CheckIfGameIsOver();
+    InitializeShelves();
+  }
+
+  private void CheckIfGameIsOver()
+  {
+    foreach(var player in StaticPlayers.Players)
+    {
+      if (player.PlayerScore >= WinCondition)
+      {
+        StaticPlayers.Winner = player;
+        SceneManager.LoadScene("WinScreen");
+      }
+    }
+  }
+
+  private void InitializeShelves()
+  {
+    int playerCount = CrossSceneInformation.PlayerList.Count;
+    var yOffset = (playerShelves[1].transform.position.y - playerShelves[0].transform.position.y) * (4 - playerCount);
+    gameObject.transform.position += new Vector3(0, yOffset, 0);
+    for (int i = 0; i < playerCount; i++)
+    {
+      InitializeShelf(i);
+    }
+  }
+
+  private void InitializeShelf(int index)
+  {
+    playerShelves[index].SetActive(true);
+    playerShelves[index].GetComponent<ShelfFiller>().FillShelf(StaticPlayers.Players[index]);
+  }
+}
---
+++ TNT-LAD/Assets/Scripts/GUI/PostGameUI/ShelfFiller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShelfFiller : MonoBehaviour
+{
+  public GameObject hatMask;
+  public List<GameObject> trophyList;
+
+  public void FillShelf(PlayerData playerData)
+  {
+    EnableTrophies(playerData.PlayerScore);
+    ColorHat(playerData.PlayerColor);
+  }
+
+  private void ColorHat(Color playerColor)
+  {
+    hatMask.GetComponent<Image>().color = playerColor;
+  }
+
+  private void EnableTrophies(int playerScore)
+  {
+    if(playerScore > 3)
+    {
+      throw new NotSupportedException("Only up to three rounds supported");
+    }
+    for(int i = 0; i < playerScore; i++)
+    {
+      trophyList[i].SetActive(true);
+    }
+  }
+}
