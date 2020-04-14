7ff8c457a6263d35b40f8b13849626719ed407bd
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-04-14 21:50:49 +0200

    Added camera movement and main menu button

---
+++ TNT-LAD/Assets/Scripts/GUI/PostGameUI/WinningSceneAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningSceneAction : MonoBehaviour
+{
+  [Header("Camera")]
+  public GameObject cameraPivot;
+  public GameObject cameraTarget;
+  public float cameraMoveSpeed;
+
+  void Start()
+  {
+    LeanTween.move(Camera.main.gameObject, cameraPivot.transform.position, cameraMoveSpeed).setEaseInOutSine().setDelay(1f);
+  }
+
+  void Update()
+  {
+    Camera.main.transform.LookAt(cameraTarget.transform.position, Vector3.up);
+  }
+}
