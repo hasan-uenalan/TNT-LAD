340bb7931fb9748886cc952756b495b866f57a5e
TvRXVII <timovonrauchhaupt@gmail.com>
2020-04-16 13:35:58 +0200

    added Trackchanging

--- TNT-LAD/Assets/Scripts/Music/MusicHandler.cs
+++ TNT-LAD/Assets/Scripts/Music/MusicHandler.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class MusicHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  private AudioSource audioSource;
 
-    // Update is called once per frame
-    void Update()
+  public AudioSource MainMenuAudio;
+  public AudioSource InGameAudio;
+  public AudioSource SuddonDeathAudio;
+  public AudioSource EndScreenAudio;
+
+  public Dictionary<string, AudioSource> Tracks;
+
+  void Awake()
+  {
+    FillTracks();
+    GameObject[] handlers = GameObject.FindGameObjectsWithTag("Music");
+    if(handlers.Length>1)
     {
-        
+      Destroy(this.gameObject);
     }
+    audioSource = gameObject.GetComponent<AudioSource>();
+    DontDestroyOnLoad(this.gameObject);
+  }
+
+  public void ChangeTrack(string trackName)
+  {
+    foreach (KeyValuePair<string, AudioSource> curTrack in Tracks) 
+    {
+      if(curTrack.Key == trackName)
+      {
+        audioSource = curTrack.Value;
+        audioSource.loop = true;
+      }
+    }
+  }
+
+  private void FillTracks()
+  {
+    Tracks = new Dictionary<string, AudioSource>();
+    Tracks.Add("MainMenu", MainMenuAudio);
+    Tracks.Add("InGame", InGameAudio);
+    Tracks.Add("SuddonDeath", SuddonDeathAudio);
+    Tracks.Add("EndScreen", EndScreenAudio);
+  }
 }
