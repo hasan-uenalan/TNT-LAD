d5f4491098231586b1f15ee71e25af4aa67c315e
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-04-22 16:36:28 +0200

    Added more trailer stuff

---
+++ TNT-LAD/Assets/Scripts/TrailerEffects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerEffects : MonoBehaviour
+{
+  public GameObject wall;
+  public GameObject curveShit;
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      gameObject.SetActive(false);
+      wall.transform.position += new Vector3(0, 0.94f, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      LeanTween.move(wall, wall.transform.position + new Vector3(0, -0.94f, 0), 2.5f);
+    }
+    if (Input.GetKeyDown(KeyCode.O))
+    {
+      curveShit.GetComponent<CPC_CameraPath>().enabled = true;
+    }
+  }
+}
